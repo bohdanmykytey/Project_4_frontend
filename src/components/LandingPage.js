f376e9d97b1ce49a3bc74086cf6c3f2c7225081c
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-09-12 09:18:08 -0400

    project unscrewed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,37 +14,26 @@ class LandingPage extends Component {
             popularity: ''
 
         }
-        this.renderMovies = this.renderMovies.bind(this)
       }
       
       componentDidMount = async () => {
          const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=2f75b3be3b1abaafec8e67b4f6622bb3&language=en-US&page=1')
          console.log(response)
-         console.log(response.data)
+         console.log(response.data.results)
          this.setState({
-           movies: {
-             rated_r: '',
-             budget: '',
-             homepage: '',
-             original_title: '',
-             overwiev: '',
-             popularity: ''
-           }
+           movies: response.data.results
          })
      }
       
-    renderMovies = ()  =>  {
-        console.log('clicked')
-        this.state.movies.map(function(movies, i) {
-            return <div key={i}>{movies.original_title}</div>
-        })
-        return movies.original_title
-    }
 
   render () {
     return (
         <div>
-            {this.renderMovies()}
+            {
+              this.state.movies.map(function(movies, i) {
+                return <div className='LandingPage' key={i}>{movies.original_title}</div>
+              })
+            }
         </div>
       
     )
